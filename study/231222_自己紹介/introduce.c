71464d5ee18744470bd4e6cc52ef587ff98cbca1
Takuto Yamaguchi <yamaguchi.t@dentsu.co.jp>
2023-12-22 16:44:50 +0900

    introduce 01 firtstime

---
+++ study/231222_自己紹介/introduce.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[100];
+    char *name = "山口大空翔";
+    char *language = "日本語";
+    char *hitokoto = "まだまだ始まったばかりさ";
+
+    printf("name: %s\n", name);
+    printf("langage: %s\n", language);
+    printf("hitokoto: %s\n", hitokoto);
+
+
+    return 0;
+}
